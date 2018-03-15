9abab50b862bcc16bf51c08b11673302640b4346
James20381 <pikej307@gmail.com>
2018-03-15 16:08:21 +1100

    Attempt fix https routing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -17,6 +18,19 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
     }
 
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(UrlGenerator $url)
+    {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+    }
+
     /**
      * Register any application services.
      *
@@ -24,6 +38,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
+
 }
